dfa8b851f0cb4f2aaf2a05f5146bd37dcda73a4f
MAQNatelimono <v-nlimono@microsoft.com>
2024-11-18 11:19:58 -0800

    Update sidenav.js
    Signed-off-by: MAQNatelimono <v-nlimono@microsoft.com>

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,4 +1,4 @@
-$("#header").load("/header.html", function() {
+$("#header").load("/header", function() {
     $('#header .icon-menu-hamburger').on('click', function() {
       // console.log('Nav item clicked in header!');
         const navbar = document.getElementById('navbar-right');
